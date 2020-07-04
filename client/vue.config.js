3c0f9ad90510d4961782917a0fcbb3be02b225aa
fontanaen <50693418+fontanaen@users.noreply.github.com>
2020-07-04 15:40:22 +0200

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,8 +3,10 @@ const path = require('path')
 module.exports = {
     outputDir : path.resolve(__dirname, '../server/public'),
     devServer : {
-        '/' : {
-            target : 'http://localhost:5000'
+        proxy : {
+            '/' : {
+                target : 'http://localhost:5000'
+            }
         }
     }
 }
